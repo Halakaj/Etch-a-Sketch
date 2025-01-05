e8e0b3c2a90358a5f0738f42c49ae536e7a7df3d
halakaj <jaka.lah000@gmail.com>
2025-01-05 11:58:02 +0100

    Added event listener and event for mouseover and a reset button

--- javascript.js
+++ javascript.js
@@ -5,3 +5,13 @@ for (let i = 0; i < 256; i++){
     child.classList.add("child");
     container.appendChild(child);
 }
+const resetButton = document.getElementById("resetButton");
+resetButton.addEventListener("click", () => {
+    const children = document.querySelectorAll(".child");
+    children.forEach(child => child.style.backgroundColor = "");
+});
+container.addEventListener("mouseover", (event) => {
+    if (event.target.classList.contains("child")) {
+        event.target.style.backgroundColor = "lightblue";
+    }
+});
